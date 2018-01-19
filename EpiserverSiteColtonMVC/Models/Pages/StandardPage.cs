ceb98f5e3017b1a7f8aef4dc10dde99a15ca8d01
Colton Gardner <cgardner268@gmail.com>
2018-01-19 14:39:33 -0800

    Added a few standard pages

---
+++ EpiserverSiteColtonMVC/Models/Pages/StandardPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverSiteColtonMVC.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "c1efae11-7bae-432d-8915-e67966a92b8e", Description = "")]
+    public class StandardPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "The Page Heading.",
+            GroupName = SystemTabNames.Content,
+            Order = 0)]
+        public virtual String Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
